e9e958d6f1407ccd82cf1a7f0ea1abb8692bb95c
ZhenBryant <57821855+ZhenBryant@users.noreply.github.com>
2019-11-16 19:33:52 +0800

    wz 页面组件
    上传功能组件

---
+++ src/components/wz/Upload.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>  
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+    
+    <van-uploader v-model="fileList" multiple :max-count="9"/>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fileList: [],
+    }
+  },
+}
+</script>
+<style>
+</style>
