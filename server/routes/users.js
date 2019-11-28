60e0a31533201aad65b43e12087b83f966b6a657
wangyuanfeng000 <57821284+wangyuanfeng000@users.noreply.github.com>
2019-11-28 20:41:34 +0800

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -64,10 +64,10 @@ router.get("/inspect",(req,res)=>{
 })
 //用户退出登录
 router.get("/remove",(req,res)=>{
-  
   var session=req.session;
   session.uid=null;
   res.send({ code: 1, msg: "删除成功" });
+})
 //用户内容发表
 router.get("/circle",(req,res)=>{
   var uid = req.session.uid;
