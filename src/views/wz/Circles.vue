cb78d1bf071be5604e227fd36860ea47401226dc
ZhenBryant <1027643479@qq.com>
2019-11-29 19:05:57 +0800

    多上传内容
    11

--- src/views/wz/Circles.vue
+++ src/views/wz/Circles.vue
@@ -2,18 +2,12 @@
   <div class="container">
     <!-- 发布内容 -->
     <div v-for="(item,i) of lists" :key="i">
-      <div class="getuname">
-        {{uname}}
-      </div>
-      <div class="getpdesc">
-        <span>{{item.pdesc}}</span>
-      </div>
+      <p class="getuname">{{item.uname}}</p>
+      <p class="getpdesc">{{item.pdesc}}</p>
       <div class="img">
-        <img :src="`http://127.0.0.1:5050${item.psrc.slice(8)}`" class="show_img">
+        <img src="" class="show_img">
       </div>
-      <div class="uptime">
-        <span>{{item.upTime}}</span>
-      </div>
+      <p class="gettime">{{item.upTime}}</p>
     </div>
   </div>
 </template>
@@ -33,14 +27,16 @@ export default {
       var url = "/users/person";
       this.axios.get(url).then(res=>{
         this.uname = res.data.uname;
+        console.log(this.uname);
       })
     },
     getMessage(){
-      var url = "/users/circle";
+      var url = "/users/circles";
       this.axios.get(url)
       .then(res=>{
-        console.log(res);
+        console.log(res);                  
         this.uid=res.data.code[0].uid;
+        console.log("uid:"+uid);
         this.pdesc=res.data.code[0].pdesc;
         this.psrc=res.data.code[0].psrc.slice(8);
         console.log(this.psrc);
@@ -50,39 +46,12 @@ export default {
         this.lists = res.data.code;
         console.log(this.lists);
         console.log(this.lists.pdesc);
-        
       })
-    }
+    },
   },
   created(){
     this.getUid(),
     this.getMessage()
-  }
+  },
 }
 </script>
-<style scoped>
-  .getuname{
-    font-size: 30px;
-    font-family: "heiti";
-    margin-left:10px;
-    font-weight: bold;
-    color:blue;
-  }
-  .img{
-    margin-left: 20px;
-    margin-top: 15px;
-  }
-  .show_img{
-    width: 200px;
-    height: 200px;
-    border:1px solid black;
-  }
-  .getpdesc{
-    font-size: 20px;
-    margin-top: 10px;
-    margin-left: 20px;
-  }
-  .uptime{
-    margin-left: 75%;
-  }
-</style>
